6fbed9351e882e39bbf87be69427f634974c9e4e
Nick Keller <Nickkeller21@gmail.com>
2020-02-24 22:10:39 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+import pandas as pd
+
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
